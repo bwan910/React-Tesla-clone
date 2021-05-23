254ec45d3e4decff5c37d0e32ea318e1202defa8
bwan910 <bwan910@gmail.com>
2021-05-23 17:13:03 +0800

    Created Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Footer() {
+    return (
+        <>
+            <FooterMenu>
+                <a href="#">Tesla @ 2021</a>
+                <a href="#">Privacy & Legal</a>
+                <a href="#">Contact</a>
+                <a href="#">Career</a>
+                <a href="#">Get Newsletter</a>
+                <a href="#">News</a>
+                <a href="#">Forums</a>
+                <a href="#">Locations</a>
+
+            </FooterMenu>
+
+            <SmFooter>
+                <li><a href="#">Tesla @ 2021</a></li>
+                <li><a href="#">Privacy & Legal</a></li>
+                <li><a href="#">News</a></li>
+
+            </SmFooter>
+        </>
+
+    )
+}
+
+export default Footer
+
+
+const FooterMenu = styled.div`
+    display: flex;
+    justify-content: center;
+    padding-bottom: 30px;
+
+    a{
+        padding: 0 10px;
+        color: black;
+        font-size: 12px;
+    }
+
+    @media (max-width: 778px) {
+        display: none;
+    }
+ 
+`
+
+// This smallFooter is for small devices
+const SmFooter = styled.div`
+    display: none; 
+    li{
+        font-size: 12px;
+        list-style-type: none;  
+        padding: 5px;
+    }
+  
+    @media (max-width: 778px) {
+        display: block;    
+    }
+
+
+`
