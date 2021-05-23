f0a210be55c24acf346ba1d8f041bcad947fdf74
bwan910 <bwan910@gmail.com>
2021-05-23 17:12:48 +0800

    None

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
-      <Home></Home>
-    </div>
+    <>
+      <div className="App">
+        <Header></Header>
+        <Home></Home>
+      </div>
+    </>
   );
 }
 
