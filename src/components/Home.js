407821ec778910161e44ded7a033f521b502e6a3
bwan910 <bwan910@gmail.com>
2021-05-24 15:54:25 +0800

    Beautify

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,27 +10,31 @@ function Home() {
             <Container>
                 <Section
                     title="Model S"
-                    description="Order Online for Touchless Delivery"
+                    description="Order Online for"
+                    descButton="Touchless Delivery"
                     backgroundImg="model-s.jpg"
                     leftBtnText="Custom order"
                     rightBtnText="Existing inventory"
                 />
                 <Section
                     title="Model Y"
-                    description="Order Online for Touchless Delivery"
+                    description="Order Online for"
+                    descButton="Touchless Delivery"
                     backgroundImg="model-y.jpg"
                     leftBtnText="Custom order"
                     rightBtnText="Existing inventory"
                 />
                 <Section
                     title="Model 3"
-                    description="Order Online for Touchless Delivery"
+                    description="Order Online for"
+                    descButton="Touchless Delivery"
                     backgroundImg="model-3.jpg"
                     leftBtnText="Custom order"
                     rightBtnText="Existing inventory" />
                 <Section
                     title="Model X"
-                    description="Order Online for Touchless Delivery"
+                    description="Order Online for"
+                    descButton="Touchless Delivery"
                     backgroundImg="model-x.jpg"
                     leftBtnText="Custom order"
                     rightBtnText="Existing inventory"
